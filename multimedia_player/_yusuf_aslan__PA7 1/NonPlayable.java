c5abfc19315a58630f34db6b2240ed65481d9400
Yusuf Aslan <32713045+YusufAAslan@users.noreply.github.com>
2023-06-29 16:40:35 +0300

    Delete multimedia_player/_yusuf_aslan__PA7 1 directory

--- multimedia_player/_yusuf_aslan__PA7 1/NonPlayable.java
+++
@@ -1,6 +0,0 @@
-// Creating interface named NonPlayable to represent non-playable objects
-// With show, info methods 
-public interface NonPlayable {
-    void show();
-    void info();
-}
--- multimedia_player/_yusuf_aslan__PA7 1/Observer.java
+++
@@ -1,5 +0,0 @@
-// Creating interface named Observer to represent observers:
-// With update method
-public interface Observer {
-    void update(Object object);
-}
